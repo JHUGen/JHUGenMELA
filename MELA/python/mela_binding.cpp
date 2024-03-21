9a4305921d86a3c3abf653ae1b477c52a7c62e65
Mohit Srivastav <vir.moh@gmail.com>
2024-03-21 11:41:55 -0400

    Update mela_binding.cpp
    Forgot to add the Resonance Propagator Scheme enums to the file

--- MELA/python/mela_binding.cpp
+++ MELA/python/mela_binding.cpp
@@ -1305,7 +1305,14 @@ PYBIND11_MODULE(Mela, m) {
         .value("gZPRIME_QQ_RIGHT", gZPRIME_QQ_RIGHT)
         .value("SIZE_ZQQ", SIZE_ZQQ);
 
-    py::enum_<CouplingIndex_ZVV>(m, "CouplingIndex_ZVV")
+    py::enum_<TVar::ResonancePropagatorScheme>(m, "ResonancePropagatorScheme")
+        .value("NoPropagator", TVar::NoPropagator)
+        .value("RunningWidth", TVar::RunningWidth)
+        .value("FixedWidth", TVar::FixedWidth)
+        .value("CPS", TVar::CPS)
+        .value("AltRunningWidth", TVar::AltRunningWidth);
+ 
+     py::enum_<CouplingIndex_ZVV>(m, "CouplingIndex_ZVV")
         .value("gZPRIME_VV_1", gZPRIME_VV_1)
         .value("gZPRIME_VV_2", gZPRIME_VV_2)
         .value("SIZE_ZVV", SIZE_ZVV);
